275af11b38f19cf85904b7bc001c537ef442c8c9
Laravel Daily <info@laraveldaily.com>
2021-09-09 14:28:10 +0000

    QuickAdminPanel automatic commit

---
+++ config/panel.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'date_format'         => 'Y-m-d',
+    'time_format'         => 'H:i:s',
+    'primary_language'    => 'en',
+    'available_languages' => [
+        'en' => 'English',
+        'de' => 'German',
+        'fr' => 'French',
+        'es' => 'Spanish',
+        'sk' => 'Slovak',
+        'pl' => 'Polish',
+        'it' => 'Italian',
+        'cs' => 'Czech',
+    ],
+    'registration_default_role' => '2',
+];
---
+++ resources/lang/cs/auth.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'failed'   => 'Tyto údaje se neshodují s našimi záznamy.',
+    'password' => 'The provided password is incorrect.',
+    'throttle' => 'Příliš mnoho pokusů o přihlášení. Zkuste to prosím znovu za: sekund sekund.',
+];
