3778bf88b371007eb76660fcb086d8dc24eabf54
andrej <andrej@stylelabs.ca>
2021-09-10 21:13:49 +0200

    renio api test

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -9,14 +9,29 @@ use Illuminate\Support\Facades\Http;
 
 class WebhookController extends Controller
 {
+    const API_KEY = 'TkNqej2CkRjWsuAPWs15OkW4OpKIViM961Ffa4Ym6fds30C9IFckhWPYTVW9QOQ7';
+    const API_HOST = 'https://reenio.cz/cs/api/v1/admin';
 
-    private function getReservationDetail($reservationId) {
-        $response = Http::acceptJson()->withToken('TkNqej2CkRjWsuAPWs15OkW4OpKIViM961Ffa4Ym6fds30C9IFckhWPYTVW9QOQ7')->get('https://reenio.cz/cs/api/v1/admin/reservation/detail/' . $reservationId);
+    private function getReservationDetail($reservationId)
+    {
+        $response = Http::acceptJson()->withToken(self::API_KEY)->get(self::API_HOST . '/reservation/detail/' . $reservationId);
         Log::info(print_r($response->json(), true));
-        return $response->body();
+        return $response;
     }
+
+    private function getPlace($placeId)
+    {
+        $response = Http::acceptJson()->withToken(self::API_KEY)->get(self::API_HOST . '/resource/place/' . $placeId);
+        Log::info(print_r($response->json(), true));
+        return $response;
+    }
+
     public function test(Request $request) {
-        var_dump($this->getReservationDetail('3323722'));
+        $reservation = $this->getReservationDetail('3323722');
+        $place = $this->getPlace($reservation->json()['detail']['placeId']);
+        echo ($reservation->json());
+        echo "\n\n";
+        echo ($place->json());
     }
     public function index(Request $request) {
         $reservationId = $request->input('reservationId');
@@ -28,4 +43,6 @@ class WebhookController extends Controller
         echo 'REENIO';
         exit;
     }
+
+
 }
