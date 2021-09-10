99dc37b62162f34ad36ca50904384d4cda2c55f9
andrej <andrej@stylelabs.ca>
2021-09-10 19:55:20 +0200

    initial webhook controller

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class WebhookController extends Controller
 {
-    public function index() {
+    public function index(Request $request) {
+        $reservationId = $request->input('reservationId');
+        Log::info(print_r($request->all(), FALSE));
+        Log::info(print_r($request->input(), FALSE));
+        Log::info($reservationId);
         echo 'REENIO';
         exit;
     }
