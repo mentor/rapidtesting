3fa55aa74596d3fb1d83dadceab492cea4ebd1e1
Laravel Daily <info@laraveldaily.com>
2021-09-11 12:54:13 +0000

    QuickAdminPanel automatic commit

--- resources/views/admin/tests/create.blade.php
+++ resources/views/admin/tests/create.blade.php
@@ -14,7 +14,7 @@
                 <select class="form-control {{ $errors->has('status') ? 'is-invalid' : '' }}" name="status" id="status" required>
                     <option value disabled {{ old('status', null) === null ? 'selected' : '' }}>{{ trans('global.pleaseSelect') }}</option>
                     @foreach(App\Models\Test::STATUS_SELECT as $key => $label)
-                        <option value="{{ $key }}" {{ old('status', 'created') === (string) $key ? 'selected' : '' }}>{{ $label }}</option>
+                        <option value="{{ $key }}" {{ old('status', '0') === (string) $key ? 'selected' : '' }}>{{ $label }}</option>
                     @endforeach
                 </select>
                 @if($errors->has('status'))
