62b2ac3e91013740366f919e7e8fe79da44b7d6d
andrej <andrej@stylelabs.ca>
2021-09-11 13:38:58 +0200

    webhook testing

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -60,10 +60,10 @@ class WebhookController extends Controller
             $payload['pinrc'] = $this->getPlainValueFromCustomField($reservation->json('detail.customForms.0.fields'), 'QTQMVKQT');
             $payload['pinid'] = $this->getPlainValueFromCustomField($reservation->json('detail.customForms.0.fields'), 'JRHRFPDB');
             $payload['symptoms'] = $this->getGeneratedValueFromCustomField($reservation->json('detail.customForms.0.fields'), 'EAENGFYD', Test::SYMPTOMS_SELECT);
-            Log::info(print_r($reservation->json('detail.customForms.0.fields'), true));
-            Log::info($this->getPlainValueFromCustomField($reservation->json('detail.customForms.0.fields'), 'DXCWCJIL'));
-            //$payload['dob'] = Carbon::createFromFormat('m.d.Y', $this->getPlainValueFromCustomField($reservation->json('detail.customForms.0.fields'), 'DXCWCJIL'));
-            $payload['dob'] = Carbon::createFromFormat('m.d.Y', '02.12.1984');
+            //Log::info(print_r($reservation->json('detail.customForms.0.fields'), true));
+            //Log::info($this->getPlainValueFromCustomField($reservation->json('detail.customForms.0.fields'), 'DXCWCJIL'));
+            $payload['dob'] = Carbon::createFromFormat('d.m.Y', $this->getPlainValueFromCustomField($reservation->json('detail.customForms.0.fields'), 'DXCWCJIL'))->toDateString();
+            //$payload['dob'] = Carbon::createFromFormat('d.m.Y', '02.12.1984')->toDateString();
             $payload['start'] = $reservation->json('detail.start');
             $payload['end'] = $reservation->json('detail.end');
             $payload['status'] = $reservation->json('detail.state');
@@ -149,7 +149,7 @@ class WebhookController extends Controller
     {
         foreach ($fields as $field) {
             if ($field['key'] == $key) {
-                return $mapper[$field['valueKey']];
+                return $field['valueKey'];
             }
         }
         return '';
