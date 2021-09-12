54214adae6e7f14dea1e357637ea46c27618bf55
andrej <andrej@stylelabs.ca>
2021-09-12 15:44:05 +0200

    email

--- resources/views/emails/verify.blade.php
+++ resources/views/emails/verify.blade.php
@@ -1,14 +1,24 @@
 Dobrý deň {{ $payload->firstname }} {{ $payload->lastname }},
+
+
 <br/><br/>
 Váš výsledok vyšetrenia SARS-COV-2 zobrazíte na adrese: {{ url(route('verify', $payload->code_ref)) }}
+
+
 <br/><br/>
 @if($payload->pinrc)
 Ako heslo použite číslo rodné číslo, ktorý ste zadali počas rezervácie termínu testovania.
 @else
 Ako heslo použite číslo dokladu, ktorý ste zadali počas rezervácie termínu testovania.
 @endif
+
+
 <br/><br/>
 Na email neodpovedajte.<br/><br/>
+
+
 Ďakujeme a prajeme pekný deň,
-<br/>
+<br/><br/>
+
+
 Testy MoM
