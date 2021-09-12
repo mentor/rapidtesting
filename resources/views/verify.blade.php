908c91e521e3490cf36c1bfd0b2d48dd7ca9eb37
andrej <andrej@stylelabs.ca>
2021-09-12 05:26:13 +0200

    svg

--- resources/views/verify.blade.php
+++ resources/views/verify.blade.php
@@ -19,36 +19,64 @@
             padding-left: 10%;
             padding-top: 5%;
         }
+
+        .color-a {
+            color: #05223b;
+        }
+
+        .color-b {
+            color: #555555;
+        }
+
+        .color-c {
+            color: #ff8878;
+        }
+
+        h1 {
+            font-size: 23px;
+            line-height: 120%;
+            margin: 0;
+            padding: 0 0 0 0;
+        }
+
+        h3 {
+            font-size: 15px;
+            margin: 10px 0 0 10px;
+        }
     </style>
 </head>
 <body class="corona">
 <form method="post" enctype="multipart/form-data">
     @csrf
     <div class="box">
-        @if($isPinRC)
-            <div class="form-group row">
-                <label for="pinrc" class="col-sm-3 col-form-label">Rodné číslo</label>
-                <div class="col-sm-9">
-                    <input name="pinrc" type="text" class="form-control" id="pinrc" placeholder="123456/7890" required>
-                </div>
-                @error('pinrc')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-            </div>
-        @else
-            <div class="form-group row">
-                <label for="pinid" class="col-sm-3 col-form-label">Číslo dokladu (občiansky preukaz alebo pas)</label>
-                <div class="col-sm-9">
-                    <input name="pinid" type="text" class="form-control" id="pinid" placeholder="(občiansky preukaz alebo pas)" required>
-                </div>
-                @error('pinid')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-            </div>
-        @endif
-        <div class="form-group row">
-            <button type="submit">Odoslať</button>
+        <div class="logo-wrapper">
+            <img class="logo" alt="" src="{{ asset('images/testy_mom-logo.png') }}"/>
         </div>
+        <div class="float-right">
+            <h1>Testy MoM</h1>
+            <h3>Výsledky</h3>
+        </div>
+        <p class="paragraph">Pre zobrazenie a stiahnutie PDF certifikátu s výsledkom Vašeho testu zadajte heslo. Prístupové heslo pre stiahnutie certifikátu je Váš rezervačný kód v tvare ABC-DEF-GHI Kód Vašej rezervácie môže obsahovať písmená aj číslice a nerozlišuje medzi malými a veľkými písmenami.</p>
+
+        <div class="form">
+            @if($isPinRC)
+                <input name="pinrc" type="text" class="form-control" id="pinrc" placeholder="123456/7890" required>
+            @else
+                <input name="pinid" type="text" class="form-control" id="pinid" placeholder="(občiansky preukaz alebo pas)" required>
+            @endif
+
+            <div class="form-group row">
+                <button type="submit">Odoslať</button>
+            </div>
+        </div>
+
+        @error('pinid')
+            <div class="alert">{{ $message }}</div>
+        @enderror
+        @error('pinrc')
+            <div class="alert">{{ $message }}</div>
+        @enderror
+
     </div>
 </form>
 </body>
