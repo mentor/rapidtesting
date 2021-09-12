92e220429d7b307b2d45eff4b15e2c0600840d41
andrej <andrej@stylelabs.ca>
2021-09-12 17:46:29 +0200

    remove checkboxes

--- resources/views/admin/tests/index.blade.php
+++ resources/views/admin/tests/index.blade.php
@@ -19,7 +19,7 @@
     </div>
 
     <div class="card-body">
-        <table class=" table table-bordered table-striped table-hover ajaxTable datatable datatable-Test">
+        <table class=" table table-striped table-hover ajaxTable datatable datatable-Test">
             <thead>
                 <tr>
 {{--                    <th width="10">--}}
