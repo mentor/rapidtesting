db33cfc04ec3d15ecdc66041c9661e2248ae802d
andrej <andrej@stylelabs.ca>
2021-09-19 17:57:24 +0200

    calendar updated

--- app/Http/Controllers/Admin/SystemCalendarController.php
+++ app/Http/Controllers/Admin/SystemCalendarController.php
@@ -30,7 +30,7 @@ class SystemCalendarController extends Controller
                 }
 
                 $events[] = [
-                    'title' => trim($source['prefix'] . ' ' . $model->{$source['field']} . ' ' . $source['suffix']),
+                    'title' => trim($source['prefix'] . ' ' . $model->{$source['field']} . ' | ' . $model->firstname . ' ' . $model->lastname . ' ' . $source['suffix']),
                     'start' => $crudFieldValue,
                     'url'   => route($source['route'], $model->id),
                 ];
