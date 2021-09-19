98a27bd7097b5b91f42ec68a610aaf2e4db13c10
andrej <andrej@stylelabs.ca>
2021-09-19 16:26:17 +0200

    rework edit page

---
+++ resources/views/partials/test-user-info.blade.php
@@ -0,0 +1,9 @@
+<div class="card">
+    <div class="card-header">
+        {{ trans('global.edit') }} {{ trans('cruds.test.title_singular') }}
+    </div>
+
+    <div class="card-body">
+        PICO VOLE
+    </div>
+</div>
