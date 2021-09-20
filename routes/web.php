2ae1ee975976d11ade7664e86402f22620f9e76f
andrej <andrej@stylelabs.ca>
2021-09-20 13:29:01 +0200

    added change time webhook

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('webhook/created', 'WebhookController@created')->withoutMiddleware(VerifyCsrfToken::class);
 Route::post('webhook/status', 'WebhookController@status')->withoutMiddleware(VerifyCsrfToken::class);
+Route::post('webhook/change', 'WebhookController@change')->withoutMiddleware(VerifyCsrfToken::class);
 
 Route::get('verify/{code_ref}', 'VerifyController@verify')->name('verify');
 Route::post('verify/{code_ref}', 'VerifyController@verify');
