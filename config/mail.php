bfc7cb94b7939123307c2c18a53e5423c0cd9208
andrej <andrej@stylelabs.ca>
2021-11-08 08:29:11 +0100

    turn off SSL verification for emails

--- config/mail.php
+++ config/mail.php
@@ -50,14 +50,6 @@ return [
                     'verify_peer_name' => false,
                 ],
             ],
-            'stream' => [
-                'ssl' => [
-                    'allow_self_signed' => true,
-                    'verify_peer' => false,
-                    'verify_peer_name' => false,
-                ],
-            ],
-
         ],
 
         'ses' => [
